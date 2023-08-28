578d85f164ee6523153b30ea8c04a5d83ea5c5f7
shreyasnair02 <SHREYAS.20201ISE0085@presidencyuniversity.in>
2023-08-29 00:34:40 +0530

    added features

---
+++ README.md
@@ -0,0 +1,34 @@
+# FontInspector Chrome Extension
+
+<div style="text-align: center;">
+  <img src="./assets/logo.svg" alt="Extension Logo" width="100px">
+</div>
+The Font Tooltip Chrome Extension enhances your browsing experience by providing font information when hovering over text elements on web pages.
+
+## Features
+
+- Display the font family of text elements when hovering over them.
+- Activate and deactivate the extension with a toggle button.
+- The tooltip is positioned close to the cursor for easy viewing.
+- Lightweight and snappy tooltip that does not cause performance issues.
+
+## Installation
+
+1. Clone this repository to your local machine or download the ZIP file.
+2. Open Google Chrome and go to `chrome://extensions/`.
+3. Enable "Developer mode" in the top right corner.
+4. Click the "Load unpacked" button and select the extension folder (the one containing `manifest.json`).
+5. The extension icon should now appear in your Chrome toolbar.
+
+## Usage
+
+1. Click on the extension icon in the Chrome toolbar to open the popup.
+2. Toggle the "Activate Font Tooltip" button to enable or disable the font tooltip feature.
+3. Hover over text elements on web pages to see their font family in a tooltip.
+
+## Customization
+
+- You can customize the extension's appearance and behavior by modifying the `popup.html`, `index.scss`, and `popup.js` files.
+- Adjust the color scheme, fonts, and styles to match your preferences.
+
+## Screenshots
--- worker.js
+++ worker.js
@@ -1,9 +1,61 @@
 tooltip = document.getElementById("tooltip-ext");
-// Create a variable to store the tooltip element
+// // Create a variable to store the tooltip element
 
+// // Function to show the tooltip
+// function showTooltip(text, x, y) {
+//   tooltip.textContent = text;
+//   tooltip.style.display = "block";
+//   tooltip.style.left = x + 10 + "px"; // Adjust as needed
+//   tooltip.style.top = y + 10 + "px"; // Adjust as needed
+// }
+
+// // Function to hide the tooltip
+// function hideTooltip() {
+//   tooltip.style.display = "none";
+// }
+
+// // Event listener for mousemove
+// document.addEventListener("mousemove", fontTooltipMousemove);
+// function fontTooltipMousemove(event) {
+//   const target = event.target;
+
+//   // Check if the target is a text element
+//   if (
+//     target instanceof HTMLElement &&
+//     [
+//       "P",
+//       "H1",
+//       "H2",
+//       "H3",
+//       "H4",
+//       "H5",
+//       "H6",
+//       "SPAN",
+//       "DIV",
+//       "A",
+//       "BUTTON",
+//       "TEXTAREA",
+//       "INPUT",
+//       "LI",
+//       "TD",
+//       "TH",
+//     ].includes(target.tagName)
+//   ) {
+//     showTooltip(
+//       window.getComputedStyle(target).fontFamily,
+//       event.clientX,
+//       event.clientY
+//     );
+//   } else {
+//     hideTooltip();
+//   }
+// }
+
+// // Event listener for scroll
+// document.addEventListener("scroll", hideTooltip);
 // Function to show the tooltip
-function showTooltip(text, x, y) {
-  tooltip.textContent = "Font Family: " + text;
+function showTooltip(info, x, y) {
+  tooltip.innerHTML = info;
   tooltip.style.display = "block";
   tooltip.style.left = x + 10 + "px"; // Adjust as needed
   tooltip.style.top = y + 10 + "px"; // Adjust as needed
@@ -22,30 +74,37 @@ function fontTooltipMousemove(event) {
   // Check if the target is a text element
   if (
     target instanceof HTMLElement &&
-    [
-      "P",
-      "H1",
-      "H2",
-      "H3",
-      "H4",
-      "H5",
-      "H6",
-      "SPAN",
-      "DIV",
-      "A",
-      "BUTTON",
-      "TEXTAREA",
-      "INPUT",
-      "LI",
-      "TD",
-      "TH",
+    ![
+      "IMG",
+      "VIDEO",
+      "AUDIO",
+      "CANVAS",
+      "SVG",
+      "OBJECT",
+      "EMBED",
+      "IFRAME",
+      "SCRIPT",
+      "STYLE",
     ].includes(target.tagName)
   ) {
-    showTooltip(
-      window.getComputedStyle(target).fontFamily,
-      event.clientX,
-      event.clientY
-    );
+    const computedStyle = window.getComputedStyle(target);
+    const fontFamilyList = computedStyle.fontFamily
+      .split(",")
+      .map((font) => font.trim());
+    const fontFamiliesToShow = fontFamilyList.slice(0, 3).join(", ");
+
+    const rgbColor = computedStyle.color.match(/\d+/g).map(Number);
+    const rgbCircle = `<span style="display: inline-block; width: 15px; height: 15px; border-radius: 50%; background-color: rgb(${rgbColor[0]}, ${rgbColor[1]}, ${rgbColor[2]}); margin-right: 5px;"></span>`;
+
+    const info = `
+       Font Family: ${fontFamiliesToShow}<br>
+      Font Weight: ${computedStyle.fontWeight}<br>
+      Font Size: ${computedStyle.fontSize}<br>
+      Font Color: ${rgbCircle}${computedStyle.color}<br>
+      Line Height: ${computedStyle.lineHeight}
+    `;
+
+    showTooltip(info, event.clientX, event.clientY);
   } else {
     hideTooltip();
   }
